7c946fab50bc9c29c1927a5e595b22c646699a80
Weilin Luo <william_luo2004@yahoo.com>
2017-05-24 17:33:31 -0700

    Add IfSample.java

---
+++ src/IfSample.java
@@ -0,0 +1,22 @@
+/**
+ * Demo the if statements
+ */
+public class IfSample {
+    public static void main(String[] args) {
+        int x, y;
+
+        x = 10;
+        y = 20;
+
+        if (x < y) System.out.println("x is less than y");
+
+        x = x * 2;
+        if (x == y) System.out.println("x now equals to y");
+
+        x = x * 2;
+        if (x > y) System.out.println("x now greater than y");
+
+        // this won't display anything
+        if (x == y) System.out.println("you won't see this");
+    }
+}
