f108d0b62d42c2f4980a4ead4e8d538ccee0eb89
Weilin Luo <william_luo2004@yahoo.com>
2017-05-24 18:58:11 -0700

    Add Light.java

---
+++ src/Light.java
@@ -0,0 +1,24 @@
+/**
+ * Created by w00759936 on 5/24/2017.
+ */
+public class Light {
+    public static void main(String[] args) {
+        int lightspeed;
+        long days;
+        long seconds;
+        long distance;
+
+        // approximate speed of light in miles per second
+        lightspeed = 186000;
+
+        days = 1000;
+
+        seconds = days * 24 * 60 * 60;
+
+        distance = lightspeed * seconds;
+
+        System.out.print("In " + days);
+        System.out.print(" days light will travel about ");
+        System.out.println(distance + " miles.");
+    }
+}
