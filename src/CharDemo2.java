cf3fbedd118b20b97884cf8e2afcc38c11c3298e
Weilin Luo <william_luo2004@yahoo.com>
2017-05-25 09:57:44 -0700

    Add CharDemo.java

---
+++ src/CharDemo2.java
@@ -0,0 +1,14 @@
+/**
+ * Created by w00759936 on 5/25/2017.
+ */
+public class CharDemo2 {
+    public static void main(String[] args) {
+        char ch1;
+
+        ch1 = 'X';
+        System.out.println("ch1 contains " + ch1);
+
+        ch1++;  // increment ch1
+        System.out.println("ch1 is now " + ch1);
+    }
+}
