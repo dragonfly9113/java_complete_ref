46203303d228e05834121ce359737c6ce1007c46
Weilin Luo <william_luo2004@yahoo.com>
2017-05-24 17:42:34 -0700

    Add ForTest.java

---
+++ src/ForTest.java
@@ -0,0 +1,11 @@
+/**
+ * Created by w00759936 on 5/24/2017.
+ */
+public class ForTest {
+    public static void main(String[] args) {
+        int x;
+
+        for (x = 0; x < 10; ++x)
+            System.out.println("This is x: " + x);
+    }
+}
