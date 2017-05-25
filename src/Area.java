eb97de3bfbbdee5c86a48d9f1ef4268fc31f6e50
Weilin Luo <william_luo2004@yahoo.com>
2017-05-25 08:29:35 -0700

    Add Area.java

---
+++ src/Area.java
@@ -0,0 +1,14 @@
+/**
+ * Created by w00759936 on 5/25/2017.
+ */
+public class Area {
+    public static void main(String[] args) {
+        double pi, r, a;
+
+        r = 10.8;
+        pi = 3.1416;
+        a = pi * r * r;
+
+        System.out.println("The area of circle is: " + a);
+    }
+}
