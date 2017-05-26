9afa342eaaae029a837b7cdb385fa7fe5d6d11ad
Weilin Luo <william_luo2004@yahoo.com>
2017-05-26 13:37:35 -0700

    Add Scope.java

---
+++ src/Scope.java
@@ -0,0 +1,21 @@
+/**
+ * Created by w00759936 on 5/26/2017.
+ */
+public class Scope {
+    public static void main(String[] args) {
+        int x;  // known to all code within main
+
+        x = 10;
+        if (x == 10) {
+            int y = 20; // known only to this block
+
+            // x and y are both known here.
+            System.out.println("x and y: " + x + " " + y);
+            x = y * 2;
+        }
+        //y = 100; //Error! y not known here
+
+        // x is still known here.
+        System.out.println("x is " + x);
+    }
+}
