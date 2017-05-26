7c7a334d1cefccce9b48c585b12966287fd584ab
Weilin Luo <william_luo2004@yahoo.com>
2017-05-26 14:02:58 -0700

    Add Conversion.java

---
+++ src/Conversion.java
@@ -0,0 +1,22 @@
+/**
+ * Created by w00759936 on 5/26/2017.
+ */
+public class Conversion {
+    public static void main(String[] args) {
+        byte b;
+        int i = 257;
+        double d = 323.142;
+
+        System.out.println("\nConversion of int to byte.");
+        b = (byte) i;
+        System.out.println("i and b " + i + " " + b);
+
+        System.out.println("\nConversion of double to int.");
+        i = (int) d;
+        System.out.println("d and i " + d + " " + i);
+
+        System.out.println("\nConversion of double to byte.");
+        b = (byte) d;
+        System.out.println("d and b " + d + " " + b);
+    }
+}
